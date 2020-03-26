ea7ac54617ffbd5733cb6f0b5575bd1f175da57e
mmargel <michael.margel@shopify.com>
2020-03-26 16:02:58 -0400

    Supports proper version names from graphiql

--- app/assets/javascripts/graphiql/rails/application.js
+++ app/assets/javascripts/graphiql/rails/application.js
@@ -1,5 +1,5 @@
 //= require ./react-16.13.1
 //= require ./react-dom-16.13.1
 //= require ./fetch-0.10.1
-//= require ./graphiql-1.0.0-alpha.3
+//= require ./graphiql-1.0.0-alpha.3-headers
 //= require ./graphiql_show
